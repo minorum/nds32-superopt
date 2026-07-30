9f862c99235b004d206dee8ed7c7022237b3dff8
Michal Andrzejczak <mit.mis@gmail.com>
2026-07-31 00:49:16 +0200

    Migrate register tests to TUnit

--- tests/Nds32.Superopt.Tests/RegisterTests.cs
+++ tests/Nds32.Superopt.Tests/RegisterTests.cs
@@ -2,38 +2,41 @@ using Nds32.Superopt.Isa.Architecture;
 
 namespace Nds32.Superopt.Tests;
 
-[TestFixture]
 public sealed class RegisterTests
 {
     [Test]
-    public void Constructor_RejectsRegisterOutsideArchitectureRange()
+    public async Task Constructor_RejectsRegisterOutsideArchitectureRange()
     {
-        Assert.That(
-            () => new Register(32),
-            Throws.TypeOf<ArgumentOutOfRangeException>());
-    }
-
-    [TestCase(0, true)]
-    [TestCase(11, true)]
-    [TestCase(12, false)]
-    [TestCase(16, true)]
-    [TestCase(19, true)]
-    [TestCase(20, false)]
-    public void IsGpr4Encodable_MatchesNds32CompressedRegisterSet(byte index, bool expected)
-    {
-        Assert.That(new Register(index).IsGpr4Encodable, Is.EqualTo(expected));
+        await Assert.That(() => new Register(32))
+            .Throws<ArgumentOutOfRangeException>();
     }
 
     [Test]
-    public void RegisterMask_EnumeratesRegistersInEncodingOrder()
+    [Arguments((byte)0, true)]
+    [Arguments((byte)11, true)]
+    [Arguments((byte)12, false)]
+    [Arguments((byte)16, true)]
+    [Arguments((byte)19, true)]
+    [Arguments((byte)20, false)]
+    public async Task IsGpr4Encodable_MatchesNds32CompressedRegisterSet(
+        byte index,
+        bool expected)
+    {
+        await Assert.That(new Register(index).IsGpr4Encodable).IsEqualTo(expected);
+    }
+
+    [Test]
+    public async Task RegisterMask_EnumeratesRegistersInEncodingOrder()
     {
         RegisterMask mask = RegisterMask.Empty
             .Add(new Register(19))
             .Add(new Register(1))
             .Add(new Register(7));
 
-        Assert.That(
-            mask.Enumerate().Select(static register => register.Index),
-            Is.EqualTo(new byte[] { 1, 7, 19 }));
+        byte[] actual = mask.Enumerate()
+            .Select(static register => register.Index)
+            .ToArray();
+
+        await Assert.That(actual.SequenceEqual([1, 7, 19])).IsTrue();
     }
 }
